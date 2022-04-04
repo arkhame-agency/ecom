1339b987d5b4ffd1c50a0e2c2ba0f7eef394a898
Kamel HEMDANE <k_hemdane@hotmail.com>
2022-04-04 02:54:12 -0400

    Added CSS/JS compiled with Vendor folder.

---
+++ public/test.blade.php
@@ -0,0 +1,53 @@
+<div class="row">
+    <div class="col-md-6 col-sm-6 col-xs-12 process-images no-padding"><img
+            src="http://uniformesdelest.com/assets/images/conditions-1.jpg" alt="Remboursement"/>1
+    </div>
+    <div class="col-md-6 col-sm-6 col-xs-12 process-detail-box no-padding">
+        <h3>Remboursement</h3>
+        <p>Les remboursements seront remis par la m&ecirc;me m&eacute;thode de paiement que celle utilis&eacute;e lors
+            de l'achat initial. Les frais d'exp&eacute;dition et les taxes de livraison ne sont pas remboursables.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6 col-sm-6 col-xs-12 process-detail-box no-padding">
+        <h3>Retour de la marchandise</h3>
+        <p>Tous les articles doivent nous &ecirc;tre retourn&eacute;s tels que r&eacute;ceptionn&eacute;s, soit : dans
+            l'emballage d'origine, avec l'&eacute;tiquette et accompagn&eacute;s du re&ccedil;u de transaction. Ils ne
+            doivent pas avoir &eacute;t&eacute; port&eacute;s et les articles endommag&eacute;s ne seront pas rembours&eacute;s.</p>
+        <p>Contactez-nous au <a style="color: #fff; white-space: nowrap;"
+                                href="tel:+15149036673"><strong>1-514-903-6673</strong></a> pour obtenir votre num&eacute;ro
+            de retour dans les 30 jours suivant la r&eacute;ception de votre article. Ensuite, suivez les &eacute;tapes
+            suivantes:</p>
+        <ul>
+            <li>Sur votre facture, inscrivez la raison du retour.</li>
+            <li>Incluez la facture dans le colis (indiquez le num&eacute;ro de retour sur la facture).</li>
+            <li>Postez votre colis au 11 000 Sherbrooke est, Montr&eacute;al est, local A-31 (en indiquant, en haut
+                &agrave; gauche, votre adresse).
+            </li>
+            <li>Une fois le colis re&ccedil;u, accordez 7 jours ouvrables pour obtenir votre remboursement.</li>
+        </ul>
+        <p>Pour les &eacute;changes et les remboursements, les frais postaux sont &agrave; votre charge. Nous ne sommes
+            pas responsables des colis perdus ou envoy&eacute;s &agrave; la mauvaise adresse. Les frais d'exp&eacute;dition
+            et les taxes de livraison ne sont pas remboursables.</p>
+    </div>
+    <div class="col-md-6 col-sm-6 col-xs-12 process-images no-padding"><img
+            src="http://uniformesdelest.com/assets/images/conditions-2.jpg" alt="Retour de la marchandise"/>2
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6 col-sm-6 col-xs-12 process-images no-padding"><img
+            src="http://uniformesdelest.com/assets/images/conditions-3.jpg" alt="Garantie"/>3
+    </div>
+    <div class="col-md-6 col-sm-6 col-xs-12 process-detail-box no-padding">
+        <h3>Garantie</h3>
+        <p>Si le produit que vous avez achet&eacute; pr&eacute;sente un d&eacute;faut de conformit&eacute; apr&egrave;s
+            la livraison, nous nous engageons &agrave; vous le remplacer ou &agrave; vous le rembourser. La garantie l&eacute;gale
+            de conformit&eacute; est une garantie qui vous prot&egrave;ge contre les d&eacute;fauts de conformit&eacute;
+            qui sont pr&eacute;sents &agrave; la date de livraison du produit. Un bien qui pr&eacute;sente un d&eacute;faut
+            de conformit&eacute; doit &ecirc;tre impropre &agrave; l'usage habituellement attendu d&rsquo;un bien
+            semblable. Il peut &eacute;galement s&rsquo;agir d&rsquo;un bien qui ne correspond pas &agrave; la
+            description que vous avez lue sur le site, ou qui ne poss&egrave;de pas les qualit&eacute;s annonc&eacute;es.
+            Aucun frais de remplacement du bien, de renvoi, de pi&egrave;ces de rechange ou de main-d&rsquo;&oelig;uvre
+            ne vous sera charg&eacute;.</p>
+    </div>
+</div>
