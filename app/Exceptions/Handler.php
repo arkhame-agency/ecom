31a87d730fc8b2560a02fe032ec82077c8287726
Kamel HEMDANE <k_hemdane@hotmail.com>
2022-05-06 22:13:33 -0400

    Sentry integration

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,6 +42,9 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $e)
     {
+        if (app()->bound('sentry') && $this->shouldReport($e)) {
+            app('sentry')->captureException($e);
+        }
         parent::report($e);
     }
 
