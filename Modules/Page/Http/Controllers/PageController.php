5484b93d19e8be29ba745a10b93641b3781da869
Kamel HEMDANE <k_hemdane@hotmail.com>
2022-05-29 20:05:53 -0400

    Delete all pages related to Maison de l'aspirateur.

--- Modules/Page/Http/Controllers/PageController.php
+++ Modules/Page/Http/Controllers/PageController.php
@@ -33,50 +33,6 @@ class PageController
         return view('public.pages.show', compact('routeArray','page', 'logo', 'latestProducts'));
     }
 
-    public function requestQuotations(Request $request)
-    {
-        $logo = File::findOrNew(setting('storefront_header_logo'))->path;
-        $latestProducts = $this->latestProducts();
-
-        return view('public.pages.request_quotations_form', compact('logo', 'latestProducts'));
-    }
-
-    public function postRequestQuotations(SendRequestQuotationsPostRequest $sendRequestsPostRequest)
-    {
-        $sendRequestsPostRequest->validated();
-
-        \Mail::to(config('mail.email-addresses.email-installation'))->send(new SendRequests($sendRequestsPostRequest));
-
-        return redirect()->route('request.quotations')->with('success', trans('storefront::requests_form.your_request_has_been_sent'));
-    }
-
-    public function registrationGuarantee()
-    {
-        $logo = File::findOrNew(setting('storefront_header_logo'))->path;
-        $latestProducts = $this->latestProducts();
-
-        $provinces = State::get('CA');
-
-        return view('public.pages.registration_guarantee_form', compact('logo', 'latestProducts', 'provinces'));
-    }
-
-    public function postRegistrationGuarantee(RegisterGuaranteePostRequest $registerGuaranteePostRequest)
-    {
-
-        $registerGuaranteePostRequest->validated();
-
-        \Mail::to(config('mail.email-addresses.email-guarantee'))->send(new RegisterGuarantee($registerGuaranteePostRequest));
-
-        if ($registerGuaranteePostRequest->subscribe_to_mailchimp) {
-            Newsletter::subscribeOrUpdate($registerGuaranteePostRequest->email, ['FNAME' => $registerGuaranteePostRequest->name, 'LNAME' => $registerGuaranteePostRequest->name]);
-            if (!Newsletter::lastActionSucceeded()) {
-                return redirect()->route('registration.guarantee')->with('error', str_after(Newsletter::getLastError(), '400: '));
-            }
-        }
-
-        return redirect()->route('registration.guarantee')->with('success', trans('storefront::requests_form.your_request_has_been_sent'));
-    }
-
     private function latestProducts()
     {
         return Product::forCard()->take(5)->latest()->get()->map->clean();
--- Modules/Page/Routes/public.php
+++ Modules/Page/Routes/public.php
@@ -3,9 +3,3 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
-
-Route::get(trans('page::routes.request_quotation_url', [], locale()), 'PageController@requestQuotations')->name('request.quotations');
-Route::post(trans('page::routes.request_quotation_url', [], locale()), 'PageController@postRequestQuotations')->name('post.request.quotations');
-
-Route::get(trans('page::routes.register_guarantee_url', [], locale()), 'PageController@registrationGuarantee')->name('registration.guarantee');
-Route::post(trans('page::routes.register_guarantee_url', [], locale()), 'PageController@postRegistrationGuarantee')->name('post.registration.guarantee');
