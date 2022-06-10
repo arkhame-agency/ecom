fab2ffce14a1fdd23dd13199067f6056139aa1f4
Kamel HEMDANE <k_hemdane@hotmail.com>
2022-06-10 00:34:04 -0400

    Fix the issue to update the selling price if there is no special_price with regular price.

--- Modules/Product/Entities/Product.php
+++ Modules/Product/Entities/Product.php
@@ -122,6 +122,15 @@ class Product extends Model
      */
     protected static function booted()
     {
+        /**
+         * Update the selling price.
+         */
+        static::retrieved(function ($product) {
+            $product->withoutEvents(function () use ($product) {
+                $product->update(['selling_price' => $product->getSellingPrice()->amount()]);
+            });
+        });
+
         static::saved(function ($product) {
             if (!empty(request()->all())) {
                 $product->saveRelations(request()->all());
